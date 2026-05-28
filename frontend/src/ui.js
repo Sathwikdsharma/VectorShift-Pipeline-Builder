7608e6c29adaf2e94275f773a2302db33a1418b5
Sathwikdsharma <sathwik.deshapathi@gmail.com>
2026-05-28 11:52:26 +0530

    Fix Vercel build warning

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -123,7 +123,7 @@ export const PipelineUI = () => {
         addNode(newNode);
       }
     },
-    [reactFlowInstance]
+    [reactFlowInstance, addNode, getNodeID]
   );
 
   const onDragOver = useCallback((event) => {
